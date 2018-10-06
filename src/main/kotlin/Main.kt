0364e711ac5932e92ae2d53be8d7a3d20244712b
44x1carbon <b4079@oic.jp>
2018-10-07 00:15:49 +0900

    Hello World!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
 package com.example
 
 import io.ktor.application.*
+import io.ktor.http.ContentType
+import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.main() {
     routing {
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain)
+        }
     }
 }
