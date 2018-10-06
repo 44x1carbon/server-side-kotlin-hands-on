c84512e910609b9b860828fcee1c1c97222358e9
44x1carbon <b4079@oic.jp>
2018-10-07 00:01:37 +0900

    init step-0

--- src/main/kotlin/Task.kt
+++
@@ -1,7 +0,0 @@
-package com.example
-
-data class Task (
-        val id: Long?,
-        val name: String,
-        val done: Boolean
-)
--- src/main/kotlin/repository/TaskRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example
-
-interface TaskRepository {
-    fun all(done: Boolean? = null): List<Task>
-    fun save(task: Task): Long
-    fun findById(id: Long?): Task?
-}
