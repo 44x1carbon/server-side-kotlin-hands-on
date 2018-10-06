bd3bb9db379826b9e8bd53ede46bfed5be486717
44x1carbon <b4079@oic.jp>
2018-10-07 00:21:41 +0900

    タスクの一覧を返す、エンドポイントの作成

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,8 +7,18 @@ import io.ktor.routing.*
 
 fun Application.main() {
     routing {
+        val taskList = mutableListOf(
+                Task(1, "タスク1", false),
+                Task(2, "タスク2", false),
+                Task(3, "タスク3", true)
+        )
+
         get("/") {
             call.respondText("Hello World!", ContentType.Text.Plain)
+        }
+
+        get("/tasks") {
+            call.respond(taskList)
         }
     }
 }
